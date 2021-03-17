7e372351afd61e19539d0da1c035e9dfc5160f32
Damien de Lartigue <damien@delartigue.co.uk>
2021-03-17 13:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
+function saturdayFun(str){
+  return `This Saturday, I want to ${str}`;
+}
+
 function wrapAdjective(flair = '*'){
   return function(adj = 'special'){
     `You are ${adj}`;
